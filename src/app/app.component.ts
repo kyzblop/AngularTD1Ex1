b693757fa55677908f02e4c95dcde348cb52e703
kyzblop <jeremymoi.jd@gmail.com>
2023-03-24 14:23:11 +0100

    fin exo1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,7 +16,26 @@ export class AppComponent {
   etudiant:any[] = [
     {"nom" : "Martin", "prenom" : "Leo", "age" : 15},
     {"nom" : "Nulos", "prenom" : "Guillaume", "age" : 75},
-    {"nom" : "Dr", "prenom" : "Who", "age" : 1500}
-
+    {"nom" : "Dr", "prenom" : "Who", "age" : 1500},
+    {"nom" : "Maitre", "prenom" : "Karine", "age" : 900}
   ]
+
+  choixClasse(isFirst: boolean, isLast: boolean){
+    var ma_classe:string=""
+    if(isFirst || isLast) {
+      ma_classe="first_last"
+    }
+    return ma_classe;
+  }
+
+  choixClasse2(isEven: boolean){
+    var ma_classe2:string=""
+    if(isEven){
+      ma_classe2="pair"
+    } else {
+      ma_classe2="impair"
+    }
+    return ma_classe2;
+  }
+
 }
